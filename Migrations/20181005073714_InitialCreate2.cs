302e2436908ce46145a2523919d5314c48ee8814
Alina <mihajlukalina32@mail.com>
2018-10-15 20:05:16 +0300

    Initial commit

---
+++ Migrations/20181005073714_InitialCreate2.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFGetStarted.AspNetCore.NewDb.Migrations
+{
+    public partial class InitialCreate2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Url",
+                table: "Blogs",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Url",
+                table: "Blogs",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
---
+++ Models/Model.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EFGetStarted.AspNetCore.NewDb.Models
+{
+    public class BloggingContext : DbContext
+    {
+        public BloggingContext(DbContextOptions<BloggingContext> options)
+            : base(options)
+        { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)//задаєм конфігурацію
+        {
+            modelBuilder.Entity<Blog>()//переопределяет соглашения и заметки к данным.
+                .Property(b => b.Url)
+                .IsRequired();
+        }
+
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }//замітки до даних
+        [Required]//обовязково щось вводити щоб не було пустим
+        public string Url { get; set; }
+    }
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+}
